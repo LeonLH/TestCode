ee919f8de9f7574896132e18284b6acceab1196b
LeonLH <laohan1221@126.com>
2020-09-22 18:00:02 +0800

    daily upload testfile

--- cpptest/code.cpp
+++ cpptest/code.cpp
@@ -5,6 +5,17 @@
 using namespace std;
 int main(){
 	string sss = "测";
-	assert(sss == "\xe6\xb5\x8b");
+	// assert(sss == "\xe6\xb5\x8b");
+	try
+	{
+		assert(0);
+		cout << "hello world" << endl;
+	}
+	catch(...)
+	{
+		cout << "break!" << endl;
+	}
+	
+	
 	return 0;
 }
