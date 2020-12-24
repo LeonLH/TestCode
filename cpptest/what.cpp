f2d968dc6c41e51062347ee7ef0cee037f3b89ba
LeonLH <laohan1221@126.com>
2020-12-24 18:00:02 +0800

    daily upload testfile

---
+++ cpptest/what.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	m_strBackupDBPeriod = '2019年12月份';
+	boost::regex e("[0-9]+");
+
+	std::string::const_iterator start, end;
+	start = pInfo->m_strBackupDBPeriod.begin();
+	end = pInfo->m_strBackupDBPeriod.end();
+	boost::match_results<std::string::const_iterator> what;
+	return 0;
+}
